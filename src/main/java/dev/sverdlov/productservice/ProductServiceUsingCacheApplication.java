e4aa43eef6cb60352bafd09523aa248696c8fdff
Miracle- <abed90eg@gmail.com>
2026-06-26 18:32:13 +0300

    init commit

---
+++ src/main/java/dev/sverdlov/productservice/ProductServiceUsingCacheApplication.java
@@ -0,0 +1,11 @@
+package dev.sverdlov.productservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductServiceUsingCacheApplication{
+    public static void main(String[] args) {
+        SpringApplication.run(ProductServiceUsingCacheApplication.class, args);
+    }
+}
---
+++ src/test/java/dev/sverdlov/productservice/ProductServiceUsingCacheApplicationTests.java
@@ -0,0 +1,12 @@
+package dev.sverdlov.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ProductServiceUsingCacheApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+
+}
